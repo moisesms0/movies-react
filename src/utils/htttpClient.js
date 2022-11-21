db4a5b228a83387250db13201c2c66fb76c17195
Moisés M <moiseselquimico@gmail.com>
2022-11-21 13:43:30 +0100

    env

--- src/utils/htttpClient.js
+++ src/utils/htttpClient.js
@@ -1,10 +1,11 @@
-export function get(path){
-    return fetch("https://api.themoviedb.org/3"+path, {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMDFiZDVjMzVkYzM0MWNmYjQ3MjhhZGQ3YmI5YTNmNiIsInN1YiI6IjYzNjMxMjAxNDIwMjI4MDA3YzhhNzFlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hJLgTfLNWuT31UNAnpgh6sJivZjcUo_3bRSOJLQvdp4",
-          "Content-Type": "application/json;charset=utf-8",
-        },
-      })
-        .then((result) => result.json())
+const API = process.env.REACT_APP_API;
+const API_TOKEN = process.env.REACT_APP_API;
+
+export function get(path) {
+  return fetch(API + path, {
+    headers: {
+      Authorization: "Bearer " + API_TOKEN,
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  }).then((result) => result.json());
 }
